f2db560e9e007e722c52e77ee36c43e340d4f118
Tiago2025TGM <133977926+Tiago2025TGM@users.noreply.github.com>
2026-03-20 12:47:13 +0000

    v.2

---
+++ meu-portfolio/app/forca/(jogo da forca)paga.js
@@ -0,0 +1,10 @@
+import Game from "../components/Game"
+
+export default function Forca(){
+    return(
+        <main>
+            <h1>jogo da Forca</h1>
+            <Game/>
+        </main>
+    );
+}
